870430b21842ea8da0ca4f52ef56a60499372402
CK Helmer <caitlinhelmer@gmail.com>
2019-05-14 22:03:06 -0400

    changes to bonus files

---
+++ StarterCode/static/js/appbonus.js
@@ -0,0 +1,12 @@
+dropdown = d3.select('.dropdown')
+
+//loop through data with a forEach;
+tableData.forEach(function(whatever){
+  //append a new row for each entry
+  const dropdownButton = dropdown.append('button');
+  dropdownButton.attr('class', 'dropdown-item').attr('type', 'button')
+  //populate the rows with data (with a nested forEach)
+  // Object.entries(whatever).forEach(function([key, value]){
+  //     const cell = tablerow.append('td').text(value);
+  })
+})
