42ec4da5808f51af24ae1ccf82e2786ae6b740ef
CK Helmer <caitlinhelmer@gmail.com>
2019-05-15 23:26:20 -0400

    troubleshooting

--- StarterCode/static/js/appbonus.js
+++ StarterCode/static/js/appbonus.js
@@ -1,12 +1,34 @@
 dropdown = d3.select('.dropdown')
+const dates = [];
+const cities = [];
+const states = [];
+const countries = [];
+const shapes = [];
+const durations = [];
+const keys = [];
 
-//loop through data with a forEach;
-tableData.forEach(function(whatever){
-  //append a new row for each entry
-  const dropdownButton = dropdown.append('button');
-  dropdownButton.attr('class', 'dropdown-item').attr('type', 'button')
+// //loop through data with a forEach;
+// data.forEach(function(whatever){
+
+//     Object.keys(whatever).forEach(function(key) {
+//       if (!(key in keys)) {
+//         keys.push(key);
+//       } 
+       
+//     })
+    // if (!(datetime.i in dates)) {
+    //   dates.push(datetime.i);
+    // }
+  
   //populate the rows with data (with a nested forEach)
-  Object.entries(whatever).forEach(function([key, value]){
-       const cell = tablerow.append('td').text(value);
-  })
-})
+  // Object.entries(whatever).forEach(function([datetime, value]) {
+    // console.log(value);
+      // if (!(value in dates)) {
+      //   dates.push(value);
+      // } 
+  //     // const dropdownButton = dropdown.append('button');
+  //     //  dropdownButton.attr('class', 'dropdown-item').attr('type', 'button')
+  // })
+// })
+
+
